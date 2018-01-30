7c4bfbefe652ab9d25b4bb6a268c8087edc2d704
Waqas Alam <alamwaqas@gmail.com>
2018-01-30 15:31:29 -0800

    add doc.go

--- hack/generate.sh
+++ hack/generate.sh
@@ -1,3 +1,3 @@
 !#/bin/bash
 
-~/kube/bin/deepcopy-gen --logtostderr --v=4 --input-dirs bgp-crd/crd --go-header-file boilerplate.go.txt -O zz_generated
+~/kube/bin/deepcopy-gen --logtostderr --v=4 --input-dirs snaproute-operator/crd --go-header-file boilerplate.go.txt -O zz_generated
